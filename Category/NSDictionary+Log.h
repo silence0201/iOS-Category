00b44374bd567bd89b923f462d8b4b15f06f077a
Silence <374619540@qq.com>
2016-09-02 21:57:16 +0800

    NSDictionary+Log

---
+++ Category/NSDictionary+Log.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Log.h
+//  Category
+//
+//  Created by 杨晴贺 on 9/2/16.
+//  Copyright © 2016 silence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Log)
+
+@end
---
+++ Category/NSDictionary+Log.m
@@ -0,0 +1,35 @@
+//
+//  NSDictionary+Log.m
+//  Category
+//
+//  Created by 杨晴贺 on 9/2/16.
+//  Copyright © 2016 silence. All rights reserved.
+//
+
+#import "NSDictionary+Log.h"
+
+@implementation NSDictionary (Log)
+
+- (NSString *)descriptionWithLocale:(id)locale{
+    NSMutableString *str = [NSMutableString string];
+    
+    [str appendString:@"{\n"];
+    
+    // 遍历字典的所有键值对
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [str appendFormat:@"\t%@ = %@,\n", key, obj];
+    }];
+    
+    [str appendString:@"}"];
+    
+    // 查出最后一个,的范围
+    NSRange range = [str rangeOfString:@"," options:NSBackwardsSearch];
+    if (range.length) {
+        // 删掉最后一个,
+        [str deleteCharactersInRange:range];
+    }
+    
+    return str;
+}
+
+@end
