427b30195d624a3d074ac6545cc09bedd9a5fa75
Silence <374619540@qq.com>
2016-12-29 22:51:02 +0800

    Update

--- Foundation/NSString/NSString+JSONValue.m
+++ Foundation/NSString/NSString+JSONValue.m
@@ -23,13 +23,13 @@
 
 - (NSArray *)JSONArrayValue{
     NSError *errorJson;
-    NSArray *jsonDict = [NSJSONSerialization JSONObjectWithData:[self dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&errorJson];
+    NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:[self dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&errorJson];
     if (errorJson != nil) {
 #ifdef DEBUG
         NSLog(@"fail to get dictioanry from JSON: %@, error: %@", self, errorJson);
 #endif
     }
-    return jsonDict;
+    return jsonArray;
 }
 
 @end
