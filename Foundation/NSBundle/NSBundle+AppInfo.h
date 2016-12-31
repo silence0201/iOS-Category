02d34035053c19558e8e3f8c3ed9cd7c2a3695af
Silence <374619540@qq.com>
2016-12-31 12:52:08 +0800

    NSBundle

---
+++ Foundation/NSBundle/NSBundle+AppInfo.h
@@ -0,0 +1,23 @@
+//
+//  NSBundle+AppInfo.h
+//  Category
+//
+//  Created by 杨晴贺 on 31/12/2016.
+//  Copyright © 2016 silence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class UIImage ;
+@interface NSBundle (AppInfo)
+
++ (NSString *)bundleName ;
++ (NSString *)bundleIdentifier ;
+
++ (UInt32)versionNumber ;
++ (NSString *)version ;
+
+- (NSString*)appIconPath ;
+- (UIImage*)appIcon ;
+
+@end
---
+++ Foundation/NSBundle/NSBundle+AppInfo.m
@@ -0,0 +1,56 @@
+//
+//  NSBundle+AppInfo.m
+//  Category
+//
+//  Created by 杨晴贺 on 31/12/2016.
+//  Copyright © 2016 silence. All rights reserved.
+//
+
+#import "NSBundle+AppInfo.h"
+#import <UIKit/UIKit.h>
+
+@implementation NSBundle (AppInfo)
+
++ (NSString*)bundleName{
+    static NSString* bundleName;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        bundleName = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleNameKey];
+    });
+    
+    return bundleName;
+}
+
++ (NSString*)bundleIdentifier{
+    static NSString* bundleIdentifier;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        bundleIdentifier = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleIdentifierKey];
+    });
+    
+    return bundleIdentifier;
+}
+
++ (UInt32)versionNumber{
+    return CFBundleGetVersionNumber(CFBundleGetMainBundle());
+}
+
++ (NSString*)version{
+    CFStringRef ver = CFBundleGetValueForInfoDictionaryKey(CFBundleGetMainBundle(), kCFBundleVersionKey);
+    return (__bridge_transfer NSString*)ver;
+}
+
+- (NSString*)appIconPath{
+    NSString* iconFilename = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleIconFile"] ;
+    NSString* iconBasename = [iconFilename stringByDeletingPathExtension] ;
+    NSString* iconExtension = [iconFilename pathExtension] ;
+    
+    return [[NSBundle mainBundle] pathForResource:iconBasename ofType:iconExtension];
+}
+
+- (UIImage*)appIcon{
+    UIImage*appIcon = [[UIImage alloc] initWithContentsOfFile:[self appIconPath]] ;
+    return appIcon;
+}
+
+@end
