fea715e4ee3c4137326b17b0882b8add43f9230e
Silence <374619540@qq.com>
2017-02-16 16:58:34 +0800

    Update

--- Classes/Foundation/NSTimer/NSTimer+Block.h
+++ Classes/Foundation/NSTimer/NSTimer+Block.h
@@ -10,8 +10,26 @@
 
 @interface NSTimer (Block)
 
+
+/**
+ scheduledTimerWithBlocl
+
+ @param seconds 时间
+ @param block Block
+ @param repeats 是否重复
+ @return 返回Timer
+ */
 + (NSTimer *)scheduledTimerWithTimeInterval:(NSTimeInterval)seconds withBlock:(void (^)(NSTimer *timer))block repeats:(BOOL)repeats;
 
+
+/**
+ timerWithTimeBlock
+
+ @param seconds 时间
+ @param block Block
+ @param repeats 是否重复
+ @return 返回Timer
+ */
 + (NSTimer *)timerWithTimeInterval:(NSTimeInterval)seconds withBlock:(void (^)(NSTimer *timer))block repeats:(BOOL)repeats;
 
 @end
