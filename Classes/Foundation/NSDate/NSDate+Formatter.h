9ceb74c367cc8703749eeb49f6e5de616d163a25
Silence <374619540@qq.com>
2017-02-16 13:26:30 +0800

    update

--- Classes/Foundation/NSDate/NSDate+Formatter.h
+++ Classes/Foundation/NSDate/NSDate+Formatter.h
@@ -10,13 +10,26 @@
 
 @interface NSDate (Formatter)
 
+/// 默认格式信息对象
 +(NSDateFormatter *)formatter;
+/// 不带时间进行格式化对象
 +(NSDateFormatter *)formatterWithoutTime;
+/// 不带日期进行格式化对象
 +(NSDateFormatter *)formatterWithoutDate;
 
+/// UTCTimeZone
 -(NSString *)formatWithUTCTimeZone;
+/// LocalTimeZone
 -(NSString *)formatWithLocalTimeZone;
+
+/**
+ TimeZoneOffset
+
+ @param offset offset
+ @return String
+ */
 -(NSString *)formatWithTimeZoneOffset:(NSTimeInterval)offset;
+/// formatWithTimeZone
 -(NSString *)formatWithTimeZone:(NSTimeZone *)timezone;
 
 -(NSString *)formatWithUTCTimeZoneWithoutTime;
