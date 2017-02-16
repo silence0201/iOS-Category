00fb48d6812f91fcbc1a497479b2c6975681097a
Silence <374619540@qq.com>
2017-02-16 17:00:06 +0800

    Update

--- Classes/UIKit/UITabBarController/UITabBarController+Hide.h
+++ Classes/UIKit/UITabBarController/UITabBarController+Hide.h
@@ -12,6 +12,14 @@
 
 @property (nonatomic,getter=isTabBarHidden,readonly) BOOL tabBarHidden ;
 
+
+/**
+ 设置Tabbar隐藏
+
+ @param hidden 是否隐藏
+ @param animated 是否有动画
+ @param completion 返回回调
+ */
 - (void)setTabBarHidden:(BOOL)hidden
                animated:(BOOL)animated
              completion:(void (^)(BOOL finished))completion;
