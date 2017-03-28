93c1be1ee0b427d3c1fa795cddd9fe9401a2913d
Silence <374619540@qq.com>
2017-03-28 22:00:30 +0800

    UIWebView

---
+++ Classes/UIKit/UIWebView/UIWebView+MetaParser.h
@@ -0,0 +1,16 @@
+//
+//  UIWebView+MetaParser.h
+//  Category
+//
+//  Created by 杨晴贺 on 2017/3/28.
+//  Copyright © 2017年 silence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (MetaParser)
+
+ ///获取网页meta信息
+-(NSArray *)getMetaData;
+
+@end
---
+++ Classes/UIKit/UIWebView/UIWebView+MetaParser.m
@@ -0,0 +1,47 @@
+//
+//  UIWebView+MetaParser.m
+//  Category
+//
+//  Created by 杨晴贺 on 2017/3/28.
+//  Copyright © 2017年 silence. All rights reserved.
+//
+
+#import "UIWebView+MetaParser.h"
+
+@implementation UIWebView (MetaParser)
+
+-(NSArray *)getMetaData
+{
+    NSString *string = [self stringByEvaluatingJavaScriptFromString:@""
+                        "var json = '[';                                    "
+                        "var a = document.getElementsByTagName('meta');     "
+                        "for(var i=0;i<a.length;i++){                       "
+                        "   json += '{';                                    "
+                        "   var b = a[i].attributes;                        "
+                        "   for(var j=0;j<b.length;j++){                    "
+                        "       var name = b[j].name;                       "
+                        "       var value = b[j].value;                     "
+                        "                                                   "
+                        "       json += '\"'+name+'\":';                    "
+                        "       json += '\"'+value+'\"';                    "
+                        "       if(b.length>j+1){                           "
+                        "           json += ',';                            "
+                        "       }                                           "
+                        "   }                                               "
+                        "   json += '}';                                    "
+                        "   if(a.length>i+1){                               "
+                        "       json += ',';                                "
+                        "   }                                               "
+                        "}                                                  "
+                        "json += ']';                                       "];
+    
+    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSError*   error = nil;
+    id array = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
+    
+    if(array==nil) NSLog(@"An error occured in meta parser.");
+    return array;
+}
+
+@end
