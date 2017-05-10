a49bc0d2502216b7a5fce03c85c1ed408c885f5d
Silence <374619540@qq.com>
2017-05-10 11:34:13 +0800

    Add NSObject+AutoCoding

---
+++ Classes/Foundation/NSObject/NSObject+AutoCoding.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+AutoCoding.h
+//  Category
+//
+//  Created by 杨晴贺 on 2017/5/10.
+//  Copyright © 2017年 silence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (AutoCoding)
+
+@end
---
+++ Classes/Foundation/NSObject/NSObject+AutoCoding.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+AutoCoding.m
+//  Category
+//
+//  Created by 杨晴贺 on 2017/5/10.
+//  Copyright © 2017年 silence. All rights reserved.
+//
+
+#import "NSObject+AutoCoding.h"
+
+@implementation NSObject (AutoCoding)
+
+@end
