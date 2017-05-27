8e0ec487b196f51ce13039b3cd8e6961c47c7f27
Silence <374619540@qq.com>
2017-05-27 22:51:24 +0800

    Update

---
+++ Classes/UIKit/UIApplication/UIApplication+UIDebug.m
@@ -0,0 +1,31 @@
+//
+//  UIApplication+UIDebug.m
+//  Category
+//
+//  Created by 杨晴贺 on 2017/5/27.
+//  Copyright © 2017年 silence. All rights reserved.
+//
+
+#import "UIApplication+UIDebug.h"
+
+@implementation UIApplication (UIDebug)
+
++ (void)showDebuggingInformationOverlay {
+#if DEBUG
+    Class overlayClass = NSClassFromString(@"UIDebuggingInformationOverlay");
+    SEL overlay = NSSelectorFromString(@"overlay");
+    SEL toggleVisibility = NSSelectorFromString(@"toggleVisibility") ;
+    SEL prepareDebuggingOverlay = NSSelectorFromString(@"prepareDebuggingOverlay") ;
+    if ([overlayClass respondsToSelector:prepareDebuggingOverlay]){
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        [overlayClass performSelector:prepareDebuggingOverlay] ;
+        id obj = [overlayClass performSelector:overlay];
+        [obj performSelector:toggleVisibility] ;
+#pragma clang diagnostic pop
+    }
+    
+#endif
+}
+
+@end
