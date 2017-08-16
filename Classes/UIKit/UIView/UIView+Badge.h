52ca53f3c1303f33c380a4ebc4a9023914010133
Silence <374619540@qq.com>
2017-08-16 22:52:06 +0800

    Update

---
+++ Classes/UIKit/UIView/UIView+Badge.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Badge.h
+//  Category
+//
+//  Created by 杨晴贺 on 2017/8/16.
+//  Copyright © 2017年 silence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Badge)
+
+@end
---
+++ Classes/UIKit/UIView/UIView+Badge.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Badge.m
+//  Category
+//
+//  Created by 杨晴贺 on 2017/8/16.
+//  Copyright © 2017年 silence. All rights reserved.
+//
+
+#import "UIView+Badge.h"
+
+@implementation UIView (Badge)
+
+@end
