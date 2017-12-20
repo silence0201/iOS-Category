86d1b6cb68dc8cee5e3f50c01222011263b0410b
Silence <374619540@qq.com>
2017-12-20 23:07:29 +0800

    Update

--- Classes/UIKit/UITableView/UITableView+Placeholder.m
+++ Classes/UIKit/UITableView/UITableView+Placeholder.m
@@ -12,7 +12,7 @@
 @implementation UITableView (Placeholder)
 
 #pragma mark - setter && getter
-- (void)setShowNoData:(BOOL)showPlaceholder {
+- (void)setShowPlaceholder:(BOOL)showPlaceholder {
     objc_setAssociatedObject(self, @selector(showPlaceholder), @(showPlaceholder), OBJC_ASSOCIATION_ASSIGN);
 }
 
@@ -50,6 +50,30 @@
 
 - (UIImage *)placeholderImage {
     return objc_getAssociatedObject(self, _cmd);
+}
+
+#pragma mark -- View
+- (UILabel *)placeholderLabel {
+    UILabel *label = objc_getAssociatedObject(self, _cmd);
+    if (!label) {
+        label = [[UILabel alloc] init];
+        label.text = self.placeholderText ? : @"暂无数据,点击刷新";
+        label.font = [UIFont systemFontOfSize:11];
+        label.textAlignment = NSTextAlignmentCenter;
+        objc_setAssociatedObject(self, _cmd, label, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return label;
+}
+
+- (UIImageView *)placeholderImageView {
+    UIImageView *imageView = objc_getAssociatedObject(self, _cmd);
+    if (!imageView) {
+        imageView = [[UIImageView alloc] init];
+        imageView.image = self.placeholderImage;
+        imageView.contentMode = UIViewContentModeCenter;
+        objc_setAssociatedObject(self, _cmd, imageView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return imageView;
 }
 
 @end
