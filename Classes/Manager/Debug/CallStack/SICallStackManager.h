7544a38eee1f841be8b0e4a1054c5eb1baca2f7a
Silence <374619540@qq.com>
2018-03-09 20:07:31 +0800

    Update

--- Classes/Manager/Debug/CallStack/SICallStackManager.h
+++ Classes/Manager/Debug/CallStack/SICallStackManager.h
@@ -4,7 +4,7 @@
 //
 //  Created by Silence on 2018/3/9.
 //  Copyright © 2018年 silence. All rights reserved.
-//
+//  https://github.com/bestswifter/BSBacktraceLogger
 
 #import <Foundation/Foundation.h>
 
