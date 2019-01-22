feff5e3d0785ab6534a286b5ce26040c80bc5c00
Silence <374619540@qq.com>
2019-01-22 23:16:02 +0800

    修改手机型号判断方法

--- Classes/Macros/SIDeviceMacro.h
+++ Classes/Macros/SIDeviceMacro.h
@@ -7,19 +7,30 @@
 //
 
 // 屏幕信息
-#define IS_RETINA_SCREE                          (MAIN_SCREEN.scale > 1.0)
-#define SCREEN_WIDTH [[UIScreen mainScreen] bounds].size.width
-#define SCREEN_HEIGHT [[UIScreen mainScreen] bounds].size.height
+#define IS_RETINA_SCREE                  (MAIN_SCREEN.scale > 1.0)
+#define SCREEN_WIDTH                     [[UIScreen mainScreen] bounds].size.width
+#define SCREEN_HEIGHT                    [[UIScreen mainScreen] bounds].size.height
 
 // 设备类型
 #define IS_IPHONE         (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 #define IS_IPAD           (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
 
-#define IS_IPHONE_4 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )480 ) < DBL_EPSILON )
-#define IS_IPHONE_5 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
-#define IS_IPHONE_6 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )667 ) < DBL_EPSILON )
-#define IS_IPHONE_6PLUS ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )736 ) < DBL_EPSILON )
-#define IS_IPHONE_X ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )812 ) < DBL_EPSILON )
+// 判断设备型号
+#define kiPhone4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) && !IS_IPAD : NO)
+#define kiPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) && !IS_IPAD : NO)
+#define kiPhone6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) && !IS_IPAD : NO)
+#define kiPhone6Plus ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size) && !IS_IPAD : NO)
+#define IS_IPHONE_X ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !IS_IPAD : NO)
+#define IS_IPHONE_Xr ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) && !IS_IPAD : NO)
+#define IS_IPHONE_Xs ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !IS_IPAD : NO)
+#define IS_IPHONE_Xs_Max ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) && !IS_IPAD : NO)
+
+#define IS_IPHONE_X_ALL \
+({BOOL isPhoneX = NO;\
+if (@available(iOS 11.0, *)) {\
+isPhoneX = [[UIApplication sharedApplication] delegate].window.safeAreaInsets.bottom > 0.0;\
+}\
+(isPhoneX);})
 
 // 设备信息
 #define DEVICE_MODEL                                [CURRENT_DEVICE model]
@@ -42,11 +53,11 @@
 // 设备尺寸信息
 #define STATUS_BAR_HEIGHT [UIApplication sharedApplication].statusBarFrame.size.height
 #define NAVIGATION_BAR_HEIGHT 44
-#define STATUS_BAR_ANDNAVIGATION_BAR_HEIGHT (IS_IPHONE_X ? 88.f : 64.f)
-#define TABBAR_HEIGHT                (IS_IPHONE_X ? (49.f+34.f) : 49.f)
-#define SAFE_BOTTOM_MARGIN_V         (IS_IPHONE_X ? 34.f : 0.f)
-#define SAFE_BOTTOM_MARGIN_H         (IS_IPHONE_X ? 21.f : 0.f)
-#define SAFE_LEFTORRIGHT_MARGIN_H         (IS_IPHONE_X ? 44.f : 0.f)
+#define STATUS_BAR_ANDNAVIGATION_BAR_HEIGHT (IS_IPHONE_X_ALL ? 88.f : 64.f)
+#define TABBAR_HEIGHT                (IS_IPHONE_X_ALL ? (49.f+34.f) : 49.f)
+#define SAFE_BOTTOM_MARGIN_V         (IS_IPHONE_X_ALL ? 34.f : 0.f)
+#define SAFE_BOTTOM_MARGIN_H         (IS_IPHONE_X_ALL ? 21.f : 0.f)
+#define SAFE_LEFTORRIGHT_MARGIN_H         (IS_IPHONE_X_ALL ? 44.f : 0.f)
 #define SAFE_INSETS(view) ({UIEdgeInsets insets; if(@available(iOS 11.0, *)) {insets = view.safeAreaInsets;} else {insets = UIEdgeInsetsZero;} insets;})
 
 
